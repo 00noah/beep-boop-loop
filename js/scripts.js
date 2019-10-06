b9b395d3f70ecb4bc90d8848ce0222718bd87703
Noah Horwitz <00noah@gmail.com>
2019-10-06 12:30:58 -0700

    add first part of for loop to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// business logic
+
+numberArray = []
+
+
+for (let n = 0; n < userNumber; n + 1) {
+  ;
+};
+
+// user logic
 $(document).ready(function() {
   $("#userForm").submit(function (event) {
     event.preventDefault();
