b90bc329010a5935d4977211fb9d4fe681ca24e0
Noah Horwitz <00noah@gmail.com>
2019-10-06 14:35:16 -0700

    add for-if-else branching for ammended array

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@
 var numberArray = []
 var ammendedArray = []
 
+
+
 // USER LOGIC
 $(document).ready(function() {
   $("#userForm").submit(function (event) {
@@ -20,16 +22,30 @@ $(document).ready(function() {
       list.removeChild(list.firstChild);
     };
 
-    // LOOP THROUGH UP TO USER'S NUMBER TO PUSH TO ARRAY AND APPPEND.
+    // LOOP UP TO USER'S NUMBER TO PUSH TO ARRAY AND APPPEND.
     for (let n = 0; n <= userNumber; n += 1) {
       numberArray.push(n.toString());
       $('ul#listOutput').append('<li>' + numberArray[n] + '</li>');
     };
 
-    // TESTING AMMENDED ARRAY CONDITIONS
-    var ammendedArray = numberArray.map(function(number) {
+    for (let n = 0; n <= userNumber; n += 1) {
+      if (numberArray.includes("1")) {
+        ammendedArray.push(n.toString());
+      }
+      else if (numberArray.includes("2")) {
+        ammendedArray.push(n.toString());
+      }
+      else {
+        ammendedArray.push(n.toString());
+      };
+  };
 
-    })
+
+
+
+
+    // TESTING AMMENDED ARRAY CONDITIONS
+    // var ammendedArray = numberArray.map();
 
     // for (let n = 0; n <= numberArray.length; n += 1) {
     //   if n ===
@@ -44,5 +60,6 @@ $(document).ready(function() {
 		$(".numberArray").text(ammendedArray);
     // RESET ARRAY TO BLANK TO READY FOR FUTURE USES
     numberArray = []
-  })
+
+  });
 });
