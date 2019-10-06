a348304823f45ab01a0dd7c67a9d0b3ee19445d0
Noah Horwitz <00noah@gmail.com>
2019-10-06 13:47:51 -0700

    change number so strings in array for searching

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,48 @@
 // business logic
 
-numberArray = []
+var numberArray = []
+var ammendedArray = []
 
-
-
-
-// user logic
+// USER LOGIC
 $(document).ready(function() {
   $("#userForm").submit(function (event) {
     event.preventDefault();
+    // SHOW SCRIPT OUTPUT
     $('.scriptOutput').show();
 
+    // ACCEPT USER FIELDS
     var userNumber = parseInt($("#userInput").val());
     $(".userInput").text(userNumber);
 
-    // Get the <ul> element with id="myList"
+    // CLEAR (POSSIBLE) PREVIOUS ANSWERS IN UL
     var list = document.getElementById("listOutput");
-    // As long as <ul> has a child node, remove it
     while (list.hasChildNodes()) {
       list.removeChild(list.firstChild);
     };
 
-
+    // LOOP THROUGH UP TO USER'S NUMBER TO PUSH TO ARRAY AND APPPEND.
     for (let n = 0; n <= userNumber; n += 1) {
-      numberArray.push(n);
+      numberArray.push(n.toString());
       $('ul#listOutput').append('<li>' + numberArray[n] + '</li>');
     };
 
-    // TESTING START
-    console.log(numberArray)
-		$(".numberArray").text(numberArray);
-    // TESTING END
-    numberArray = []
+    // TESTING AMMENDED ARRAY CONDITIONS
+    var ammendedArray = numberArray.map(function(number) {
 
-    // for (var index = number1Input; index <= number2Input; index += number1Input) {
-    //   $('.answer').append(index + ", ");
+    })
+
+    // for (let n = 0; n <= numberArray.length; n += 1) {
+    //   if n ===
     // };
 
 
 
-
-
+    // TESTING START
+    console.log(numberArray)
+		$(".numberArray").text(numberArray);
+    console.log(ammendedArray)
+		$(".numberArray").text(ammendedArray);
+    // RESET ARRAY TO BLANK TO READY FOR FUTURE USES
+    numberArray = []
   })
 });
